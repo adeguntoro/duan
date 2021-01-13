95e2a8394b2995fcc54bbd1b86f7629e6c432fd1
ade guntoro <guntoroade@gmail.com>
2021-01-13 17:41:11 +0700

    first commit

---
+++ app/Http/Controllers/DuanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Duan;
+use Illuminate\Http\Request;
+
+class DuanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Duan  $duan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Duan $duan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Duan  $duan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Duan $duan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Duan  $duan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Duan $duan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Duan  $duan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Duan $duan)
+    {
+        //
+    }
+}
